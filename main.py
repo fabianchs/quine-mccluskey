60151404b9bbb585a5f6492a1da35ed62804db39
chackymeow <fabichasola@hotmail.com>
2022-08-25 00:11:43 +0000

    start comparison algorithm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("hello world")
-print("this is the first file for the project")
-print("Hello world and viva saprissa")
-print("Tuanis?")
-print("Arriba Costa Rica")
+
+
+#Función que compara los valores repetidos
+
+#la siguiente lista es temporal, para efectos de comprobar el funcionamiento del algoritmo
+#los mintérminos insertados son sumatoria de m(1,3,4,5,9,11,12,13,14,15)
+
+list= [[1,0,0,0,1],[3,0,0,1,1],[4,0,1,0,0],[5,0,1,0,1],[9,1,0,0,1],[11,1,0,1,1],[12,1,1,0,1],[13,1,1,0,1],[14,1,1,1,0],[15,1,1,1,1]]
+
+print(list)
