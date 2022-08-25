70ae015e9c4a80053b3e76480bf17808b259b350
chackymeow <fabichasola@hotmail.com>
2022-08-25 01:08:31 +0000

    comparison function done

--- main.py
+++ main.py
@@ -5,24 +5,30 @@ from collections import Counter
 #la siguiente lista es temporal, para efectos de comprobar el funcionamiento del algoritmo
 #los mintérminos insertados son sumatoria de m(1,3,4,5,9,11,12,13,14,15)
 
-list_to_compare= [[1,0,0,0,1],[3,0,0,1,1],[4,0,1,0,0],[5,0,1,0,1],[9,1,0,0,1],[11,1,0,1,1],[12,1,1,0,1],[13,1,1,0,1],[14,1,1,1,0],[15,1,1,1,1]]
+list_to_compare= [[1,0,0,0,1],[3,0,0,1,1],[4,0,1,0,0],[5,0,1,0,1],[9,1,0,0,1],[11,1,0,1,1],[12,1,1,0,0],[13,1,1,0,1],[14,1,1,1,0],[15,1,1,1,1]]
 
 for value in list_to_compare:
     print(value)
 
-#función para agrupar en términos de 1
+#función para agrupar en términos de 1's
 
 def compare(list_to_compare):
-    zeros_count=1
     sorted_dict={}
 
     for values in list_to_compare:
-     
-        counter = Counter(values)
-        print(counter)
-            
+        consider=values[1:5]
+        counter = Counter(consider)
+
+        if counter[1] not in sorted_dict:
+            sorted_dict[counter[1]]=[values]
+        elif counter[1] in sorted_dict:
+            new_list= sorted_dict[counter[1]]
+            new_list.append(values)
+            sorted_dict[counter[1]]=new_list
     
-    sorted_dict[str(zeros_count)]="insert sorted values"
+    print(sorted_dict)
+
+
 
     return 0
 
