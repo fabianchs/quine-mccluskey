f62b6e73c97ef077cfbc6a553b5964d19f04ce3a
Willysan23 <william.asr23@gmail.com>
2022-08-26 23:36:14 +0000

    Creo que ya esta

--- Read min.py
+++ Read min.py
@@ -2,13 +2,23 @@
 
 #Con el comando "with" se utiliza para que el archivo se cierre una vez que se termina de utilizar el mismo.
 #Se abre el archivo como file para que a la hora de realizar 
-with open("list.txt") as file:
-    min = file.read()
-    print(min)
 
-max = min
-for index in range ( len ( max ) ):
-    if max[index] != ",":
-        print ( max[index])
+with open("list.txt") as mt:
+    minterms = mt.read().split(',')
 
- 
+print(minterms)
+for i in minterms:
+    bin = format(int(i), "b")
+    print('El mintermino es: ', i,'Equivale a: ',bin)
+
+
+
+
+
+
+
+
+
+
+
+
