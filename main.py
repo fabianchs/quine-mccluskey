fe5297cfe381f2746115c4ee74ba4acf9cfd5c3c
JohanelSteve <johanelsteve@gmail.com>
2022-08-27 03:34:14 +0000

    clean

--- main.py
+++ main.py
@@ -37,60 +37,13 @@ def compare(list_to_compare):
 
 
 #la siguiente función comparará los grupos previamente ordenados por cantidad de 1s
-def compare_groups(groups_to_compare):
-    key_to_compare=1
-    quant_of_comparisons=list(groups_to_compare.keys()) #no se puede ir de 1 en 1 con la cantidad de ceros
-    #porque puede existir una expresión que no tenga 2 unos
-    print (quant_of_comparisons,"HERE")
-    groups=[]
-
-    for i in quant_of_comparisons:
-        for group in groups_to_compare[i]:
-            #estamos recorriendo los de la cant de 1s
-            print(i,"group")
-            actual_bin_position=group[1:5]
-            
-            for row in groups_to_compare[i]:
-                diff_count=0
-                diff_position=None
-                consider_from_row=row[1:5]
-                print('i',i,'group',group,'row',row)
-                if group==row:
-                    pass 
-                else:
-                    for position in range(0,3):
-                        if consider_from_row[position]!=actual_bin_position[position]:
-                            diff_count+=1
-                            diff_position=position
-                        
-                    if diff_count==1:
-                        insert=consider_from_row
-                        insert[position]='x'
-                        groups.append([[group[0],row[0]],insert])
-                        diff_count=0
-                        diff_position=None
 
 
-
-                
-                #if i==quant_of_comparisons[0]:
-                 #   print("salto",i,quant_of_comparisons[0])
-                #else:
-                 #   consider= row[1:5]
-                  #  print(consider,"row")
-                #print(set(row).difference(set(groups_to_compare[key_to_compare+2]))) #retorna la cantidad de diferencias entre los binarios
-                #llave 1 la comparamos con grupo de llave siguiente 
-        print("group change")
-        key_to_compare+=1
-        print(groups)
-
-        
     #con el fin de poder agrupar correctamente los términos que tienen diferencias 
     #es necesario crear una lista con listas, primera lista contiene los mintérminos, segunda lista contiene los binarios
     #segunda lista, tiene otra lista interna con los binarios separados ejm lista=[[[1,3],[1,0,0,1],[0,1,1,0]]]
   
 
-    return 0
 
 def compare_groups2(groups_to_compare):
     comparison_keys=list(groups_to_compare.keys())
---
+++ tests.py
@@ -0,0 +1,25 @@
+def converter(list): #funcion para convertir los valores hexadecimales a binarios
+    
+
+    
+
+
+
+    return list_to_compare #esta es la salida que resive la funcion "" 
+
+
+#Funcion para abrir y leer el archivo de texto
+
+#Con el comando "with" se utiliza para que el archivo se cierre una vez que se termina de utilizar el mismo.
+#Se abre el archivo como file para que a la hora de realizar 
+
+with open("list.txt") as mt:
+    minterms = mt.read().split(',')
+
+print(minterms)
+for i in minterms:
+    bin = format(int(i), "b")
+    print('El mintermino es: ', i,'Equivale a: ',bin)
+
+
+
