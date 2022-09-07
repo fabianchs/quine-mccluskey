35f8a268b278967643d163fee949eaeeffd56fec
chackymeow <fabichasola@hotmail.com>
2022-09-07 19:42:38 +0000

    read txt works

--- Read min.py
+++ Read min.py
@@ -4,30 +4,23 @@
 #Se abre el archivo como file para que a la hora de realizar 
 minterms=""
 
-with open("list.txt","r") as mt:
-    literals = mt.readline(1)
-    minterms = mt.readlines(2)
-    minterms=minterms[1]
-    minterms=list(minterms)
+def read():
+    with open("list.txt","r") as mt:
+        literals = mt.readline(1)
+        minterms = mt.readlines(2)
+        minterms=minterms[1]
+        minterms=minterms.split(',')
 
-    final=[]
-    
-    for i in minterms:
-        if i!=",":
-            final.append(format(int(i)))
+        final=[]
+        
+        for i in minterms:
+            final.append((int(i)))
+        
+        minterms=final
+        print(minterms)
+        return minterms
 
-print(literals,"\n",minterms)
-"""
-for i in minterms:
-    bin = format(int(i), "b")
-    print('El mintermino es: ', i,'Equivale a: ',bin)
-
-"""
-"4,23,64"
-"4 a binario, un append a la lista entonces queda [mintérmino, lista de binarios]"
-"bin(4)"
-
-
+read()
 
 
 
