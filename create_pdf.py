69088f6b7aa809f67380bfe89f7e4dac942b468e
chackymeow <fabichasola@hotmail.com>
2022-09-07 22:26:05 +0000

    creates pdf

--- create_pdf.py
+++ create_pdf.py
@@ -10,8 +10,8 @@ import os
 if __name__ == '__main__':
     image_filename = os.path.join(os.path.dirname(__file__), 'kitten.jpg')
 
-    geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
-    doc = Document(geometry_options=geometry_options)
+    geometry_options = {"tmargin": "1cm", "lmargin": "1cm"}
+    doc = Document(geometry_options=geometry_options, documentclass="beamer")
 
     with doc.create(Section('The simple stuff')):
         doc.append('Some regular text and some')
