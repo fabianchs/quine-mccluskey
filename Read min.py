f64b683ad409d5cd794ef96599db09115901b9aa
chackymeow <fabichasola@hotmail.com>
2022-09-07 16:23:41 +0000

    working on txt reading

--- Read min.py
+++ Read min.py
@@ -2,12 +2,22 @@
 
 #Con el comando "with" se utiliza para que el archivo se cierre una vez que se termina de utilizar el mismo.
 #Se abre el archivo como file para que a la hora de realizar 
+minterms=""
 
+with open("list.txt","r") as mt:
+    literals = mt.readline(1)
+    minterms = mt.readlines(2)
+    minterms=minterms[1]
+    minterms=list(minterms)
+
+    final=[]
+    
+    for i in minterms:
+        if i!=",":
+            final.append(format(int(i)))
+
+print(literals,"\n",minterms)
 """
-with open("list.txt") as mt:
-    minterms = mt.read().split(',')
-
-print(minterms)
 for i in minterms:
     bin = format(int(i), "b")
     print('El mintermino es: ', i,'Equivale a: ',bin)
@@ -17,10 +27,6 @@ for i in minterms:
 "4 a binario, un append a la lista entonces queda [mintérmino, lista de binarios]"
 "bin(4)"
 
-literals=7
-minterms=[[4,0,1,0,0],[23,1,0,1,1,0],[64,1,0,0,0,0,0,0]]
-
-print (minterms)
 
 
 
